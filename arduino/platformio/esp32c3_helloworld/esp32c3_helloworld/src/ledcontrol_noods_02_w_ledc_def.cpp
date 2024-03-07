95556d79442001970077c8bb8721cc94503cd9b1
Adriaan Wormgoor <adriaan@adriaanwormgoor.nl>
2024-03-07 16:46:17 +0100

    update noods test sketch

--- arduino/platformio/esp32c3_helloworld/esp32c3_helloworld/src/ledcontrol_noods_02_w_ledc_def.cpp
+++ arduino/platformio/esp32c3_helloworld/esp32c3_helloworld/src/ledcontrol_noods_02_w_ledc_def.cpp
@@ -4,14 +4,18 @@
 // Exploring using direct ledc register control (instead of analogWrite)
 //
 
-#define nood1a D1
-#define nood1b D10
+#define nood1a D10
+#define nood1b D1
+#define nood2a D8
+#define nood2b D0
 
 const uint8_t nood1a_chan = 0;
 const uint8_t nood1b_chan = 1;
+const uint8_t nood2a_chan = 2;
+const uint8_t nood2b_chan = 3;
 
 // setting PWM properties
-const uint32_t freq = 1000;
+const uint32_t freq = 2500;
 const uint8_t resolution = 8;
 
 // to prevent the motors from running
@@ -20,13 +24,8 @@ const uint8_t resolution = 8;
 #define motor2a_pin D3
 #define motor2b_pin D2
 
-const uint8_t motor1a_chan = 2;
-const uint8_t motor1b_chan = 3;
-const uint8_t motor2a_chan = 4;
-const uint8_t motor2b_chan = 5;
-
-#define targLedcPin nood1b
-#define targLedcChan nood1b_chan
+const uint8_t motor1_chan = 4;
+const uint8_t motor2_chan = 5;
 
 void setup()
 {
@@ -37,6 +36,8 @@ void setup()
   // not sure if this is necessary...
   ledcDetachPin(nood1a);
   ledcDetachPin(nood1b);
+  ledcDetachPin(nood2a);
+  ledcDetachPin(nood2b);
   ledcDetachPin(motor1a_pin);
   ledcDetachPin(motor1a_pin);
   ledcDetachPin(motor1b_pin);
@@ -55,25 +56,10 @@ void setup()
   digitalWrite(motor2b_pin, 0);
   //*/
 
-  /*
-  ledcSetup(motor1a_chan, freq, resolution);
-  ledcAttachPin(motor1a_pin, motor1a_chan);
-  ledcSetup(motor1b_chan, freq, resolution);
-  ledcAttachPin(motor1b_pin, motor1b_chan);
-  ledcSetup(motor2a_chan, freq, resolution);
-  ledcAttachPin(motor2a_pin, motor2a_chan);
-  ledcSetup(motor2b_chan, freq, resolution);
-  ledcAttachPin(motor2b_pin, motor2b_chan);
-
-  ledcWrite(motor1a_chan, 127);
-  ledcWrite(motor1b_chan, 127);
-  ledcWrite(motor2a_chan, 127);
-  ledcWrite(motor2b_chan, 127);
-  //*/
-
-  // pinMode(targLedcPin, OUTPUT);
   pinMode(nood1a, OUTPUT);
   pinMode(nood1b, OUTPUT);
+  pinMode(nood2a, OUTPUT);
+  pinMode(nood2b, OUTPUT);
 
   ledcSetup(nood1a_chan, freq, resolution);
   ledcAttachPin(nood1a, nood1a_chan);
@@ -81,44 +67,88 @@ void setup()
   ledcSetup(nood1b_chan, freq, resolution);
   ledcAttachPin(nood1b, nood1b_chan);
 
-  Serial.println("main_ledcontrol_noods_w_ledc_def - hello");
+  ledcSetup(nood2a_chan, freq, resolution);
+  ledcAttachPin(nood2a, nood2a_chan);
+
+  ledcSetup(nood2b_chan, freq, resolution);
+  ledcAttachPin(nood2b, nood2b_chan);
+
+  Serial.println("ledcontrol_noods_02_w_ledc_def - hello");
 }
 
+uint16_t delayTime = 2500;
 void loop()
 {
-  //*
-  ledcWrite(nood1a_chan, 255);
-  ledcWrite(nood1b_chan, 220);
-  delay(200);
-  ledcWrite(nood1b_chan, 127);
-  delay(200);
-  ledcWrite(nood1b_chan, 0);
-  delay(500);
-
+  // nood1a on
+  Serial.println("nood1a on");
+  ledcWrite(nood1a_chan, 0);
   ledcWrite(nood1b_chan, 255);
-  ledcWrite(nood1a_chan, 220);
-  delay(200);
-  ledcWrite(nood1a_chan, 127);
-  delay(200);
+  ledcWrite(nood2a_chan, 255);
+  ledcWrite(nood2b_chan, 255);
+
+  delay(delayTime);
+
+  // nood1b on
+  Serial.println("nood1b on");
+  ledcWrite(nood1a_chan, 255);
+  ledcWrite(nood1b_chan, 0);
+  ledcWrite(nood2a_chan, 255);
+  ledcWrite(nood2b_chan, 255);
+
+  delay(delayTime);
+
+  // nood2a on
+  Serial.println("nood2a on");
+  ledcWrite(nood1a_chan, 255);
+  ledcWrite(nood1b_chan, 255);
+  ledcWrite(nood2a_chan, 0);
+  ledcWrite(nood2b_chan, 255);
+
+  delay(delayTime);
+
+  // nood2b on
+  Serial.println("nood2b on");
+  ledcWrite(nood1a_chan, 255);
+  ledcWrite(nood1b_chan, 255);
+  ledcWrite(nood2a_chan, 255);
+  ledcWrite(nood2b_chan, 0);
+
+  delay(delayTime);
+
+  // nood1a+b on
+  Serial.println("nood1a+b on");
   ledcWrite(nood1a_chan, 0);
-  delay(500);
-  //*/
+  ledcWrite(nood1b_chan, 0);
+  ledcWrite(nood2a_chan, 255);
+  ledcWrite(nood2b_chan, 255);
 
-  /*
-  ledcWrite(nood1a_chan, 220);
-  delay(200);
-  ledcWrite(nood1a_chan, 127);
-  delay(200);
+  delay(delayTime);
+
+  // nood2a+b on
+  Serial.println("nood2a+b on");
+  ledcWrite(nood1a_chan, 255);
+  ledcWrite(nood1b_chan, 255);
+  ledcWrite(nood2a_chan, 0);
+  ledcWrite(nood2b_chan, 0);
+
+  delay(delayTime);
+
+  // noods all on
+  Serial.println("noods on");
   ledcWrite(nood1a_chan, 0);
-  delay(500);
+  ledcWrite(nood1b_chan, 0);
+  ledcWrite(nood2a_chan, 0);
+  ledcWrite(nood2b_chan, 0);
 
-  ledcWrite(nood1a_chan, 32);
-  delay(200);
-  ledcWrite(nood1a_chan, 127);
-  delay(200);
-  ledcWrite(nood1a_chan, 220);
-  delay(500);
-  //*/
+  delay(delayTime);
 
-  Serial.println(millis());
+  // all off
+  Serial.println("noods off");
+  ledcWrite(nood1a_chan, 255);
+  ledcWrite(nood1b_chan, 255);
+  ledcWrite(nood2a_chan, 255);
+  ledcWrite(nood2b_chan, 255);
+
+  delay(delayTime);
+
 }
