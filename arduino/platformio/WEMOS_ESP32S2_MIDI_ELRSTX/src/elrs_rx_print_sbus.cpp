ea3911d1fe40ef36d2de396a19f3ef175295de6d
Adriaan Wormgoor <adriaan@adriaanwormgoor.nl>
2024-05-03 11:13:32 +0200

    add simple sbus rx print sketch

---
+++ arduino/platformio/WEMOS_ESP32S2_MIDI_ELRSTX/src/elrs_rx_print_sbus.cpp
@@ -0,0 +1,90 @@
+#include <Arduino.h>
+#include <Wire.h>
+
+#include "sbus.h"
+#include <HardwareSerial.h>
+
+HardwareSerial MySerial0(0);
+
+/* SBUS object, reading SBUS */
+bfs::SbusRx sbus_rx(&MySerial0, 39, 37, true, false);
+/* SBUS object, writing SBUS */
+// bfs::SbusTx sbus_tx(&Serial2);
+/* SBUS data */
+bfs::SbusData data;
+
+#define SBUS_PACKET_PRINT_INTERVAL 1000/100 // ms
+u_long sbusPacketPrintPrevTime = 0;
+
+u_long sbusPrevPacketTime;
+bool sbusLost = false;
+
+#define SBUS_LOST_TIMEOUT 100
+
+void parseSBUS(bool serialPrint);
+
+void setup() {
+  /* Serial to display data */
+  Serial.begin(115200);
+  while (!Serial) {}
+  /* Begin the SBUS communication */
+  sbus_rx.Begin();
+  // sbus_tx.Begin();
+
+  Serial.println("SBUS RX Test - START");
+}
+
+void loop () {
+  parseSBUS(true);
+
+  sbusPrevPacketTime = millis();
+
+  delay(1000/150);
+}
+
+void parseSBUS(bool serialPrint)
+{
+  if (sbus_rx.Read())
+  {
+    sbusPrevPacketTime = millis();
+    if (sbusLost)
+    {
+      Serial.println("SBUS connection restored");
+      sbusLost = false;
+    }
+
+    /* Grab the received data */
+    data = sbus_rx.data();
+
+    /* Display the received data */
+    if (millis() - sbusPacketPrintPrevTime > SBUS_PACKET_PRINT_INTERVAL)
+    {
+      //*
+      if (Serial && serialPrint)
+      {
+        for (int8_t i = 0; i < data.NUM_CH; i++)
+        {
+          Serial.print(data.ch[i]);
+          Serial.print("\t");
+        }
+        Serial.println();
+      }
+      //*/
+
+      sbusPacketPrintPrevTime = millis();
+    }
+  }
+
+  // if SBUS lost, reset the channels
+  if (millis() - sbusPrevPacketTime > SBUS_LOST_TIMEOUT)
+  {
+    if (!sbusLost)
+    {
+      Serial.println("SBUS connection lost");
+
+      sbusLost = true;
+    }
+  }
+
+}
+
