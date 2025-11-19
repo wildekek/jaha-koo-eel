ab39e97c267923d4e51346fbd3476efa0d09353b
Adriaan Wormgoor <adriaan@adriaanwormgoor.nl>
2025-11-19 14:15:53 +0100

    Update main_sbusreceiver.cpp

--- arduino/platformio/esp32c3_helloworld/esp32c3_helloworld/src/main_sbusreceiver.cpp
+++ arduino/platformio/esp32c3_helloworld/esp32c3_helloworld/src/main_sbusreceiver.cpp
@@ -8,30 +8,88 @@ HardwareSerial MySerial0(0);
 
 /* SBUS object, reading SBUS */
 bfs::SbusRx sbus_rx(&MySerial0, D7, D6, true, false);
+
 /* SBUS object, writing SBUS */
 // bfs::SbusTx sbus_tx(&Serial2);
+
 /* SBUS data */
 bfs::SbusData data;
+
+#define SBUS_PACKET_PRINT_INTERVAL 1000/100 // ms
+u_long sbusPacketPrintPrevTime = 0;
+
+u_long sbusPrevPacketTime;
+bool sbusLost = false;
+
+#define SBUS_LOST_TIMEOUT 100
+
+void parseSBUS(bool serialPrint);
 
 void setup() {
   /* Serial to display data */
   Serial.begin(115200);
-  while (!Serial) {}
+  
+  pinMode(D7, INPUT_PULLUP);
+
+  // while (!Serial) {}
+  delay(2000);
+
   /* Begin the SBUS communication */
   sbus_rx.Begin();
   // sbus_tx.Begin();
 }
 
 void loop () {
-  if (sbus_rx.Read()) {
+  parseSBUS(true);
+
+  sbusPrevPacketTime = millis();
+
+  delay(1000/150);
+}
+
+void parseSBUS(bool serialPrint)
+{
+  if (sbus_rx.Read())
+  {
+    sbusPrevPacketTime = millis();
+    if (sbusLost)
+    {
+      Serial.println("SBUS connection restored");
+      sbusLost = false;
+    }
+
     /* Grab the received data */
     data = sbus_rx.data();
+
     /* Display the received data */
-    for (int8_t i = 0; i < data.NUM_CH; i++) {
-      Serial.print(data.ch[i]);
-      Serial.print("\t");
+    if (millis() - sbusPacketPrintPrevTime > SBUS_PACKET_PRINT_INTERVAL)
+    {
+      //*
+      if (Serial && serialPrint)
+      {
+        for (int8_t i = 0; i < data.NUM_CH; i++)
+        {
+          Serial.print(data.ch[i]);
+          Serial.print("\t");
+        }
+        Serial.println();
+      }
+      //*/
+
+      sbusPacketPrintPrevTime = millis();
     }
-    Serial.println();
   }
-  delay(100);
+
+  // if SBUS lost, reset the channels
+  if (millis() - sbusPrevPacketTime > SBUS_LOST_TIMEOUT)
+  {
+    if (!sbusLost)
+    {
+      Serial.println("SBUS connection lost");
+
+      sbusLost = true;
+    }
+  }
+
 }
+
